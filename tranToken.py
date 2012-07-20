7cecb0adc4eca4c5038fee136f9af312f98b0958
Zhengsheng Zhou <edwardbadboy@hotmail.com>
2012-07-21 01:18:23 +0800

    add support for sophisticated subject partial applying
    Signed-off-by: Zhengsheng Zhou <edwardbadboy@hotmail.com>

--- tranToken.py
+++ tranToken.py
@@ -6,7 +6,11 @@ import sys
 from pprint import pprint
 
 _NORMAL = 0
-_INPARTIAL = 1
+_PARTIAL_SUBJECT = 1
+_UNTIL_LEFT_PARENTHESIS = 2
+_PARTIAL_IN_PARENTHESES = 3
+_UNTIL_RIGHT_PARENTHESIS = 4
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -21,22 +25,92 @@ if __name__ == '__main__':
 
             result = []
             st = _NORMAL
+            subject_lp = 0
+            paren_lp = 0
+            st_stack = []
+
             for i, tk in enumerate(tokens):
                 tktype, tkstr, tkstart, tkend, tkline = tk
                 #pprint((tok_name[tktype], tkstr))
-                if st == _INPARTIAL:
+                if st == _PARTIAL_SUBJECT:
                     if (tktype, tkstr) == (OP, '('):
-                        result.append((OP, ','))
-                        st = _NORMAL
+                        result.append((tktype, tkstr))
+                        subject_lp = 1
+                        st = _PARTIAL_IN_PARENTHESES
                     else:
                         result.append((tktype, tkstr))
-                elif tktype == ERRORTOKEN and tkstr == '$':
-                    result.extend(
-                            [(NAME, 'superPartial'),
-                             (OP, '(')])
-                    st = _INPARTIAL
+                        st = _UNTIL_LEFT_PARENTHESIS
+
+                elif st == _PARTIAL_IN_PARENTHESES:
+                    if (tktype, tkstr) == (OP, '('):
+                        result.append((tktype, tkstr))
+                        subject_lp += 1
+                    elif (tktype, tkstr) == (OP, ')'):
+                        subject_lp -= 1
+                        if subject_lp < 0:
+                            raise Exception(
+                                    "Parentheses don't match on line "
+                                    "%d, col %d" % tkline, tkstart)
+                        result.append((tktype, tkstr))
+                        if subject_lp == 0:
+                            st = _UNTIL_LEFT_PARENTHESIS
+                    elif (tktype, tkstr) == (ERRORTOKEN, '$'):
+                        result.extend(
+                                [(NAME, 'superPartial'),
+                                 (OP, '(')])
+                        st_stack.append((st, subject_lp, paren_lp))
+                        subject_lp = 0
+                        paren_lp = 0
+                        st = _PARTIAL_SUBJECT
+                    else:
+                        result.append((tktype, tkstr))
+
+                elif st == _UNTIL_LEFT_PARENTHESIS:
+                    if (tktype, tkstr) == (OP, '('):
+                        result.append((OP, ','))
+                        paren_lp = 1
+                        st = _UNTIL_RIGHT_PARENTHESIS
+                    else:
+                        result.append((tktype, tkstr))
+
+                elif st == _UNTIL_RIGHT_PARENTHESIS:
+                    if (tktype, tkstr) == (OP, '('):
+                        result.append((tktype, tkstr))
+                        paren_lp += 1
+                    elif (tktype, tkstr) == (OP, ')'):
+                        paren_lp -= 1
+                        if paren_lp < 0:
+                            raise Exception(
+                                    "Parentheses don't match on line "
+                                    "%d, col %d" % tkline, tkstart)
+                        result.append((tktype, tkstr))
+                        if paren_lp == 0:
+                            st, subject_lp, paren_lp = st_stack.pop()
+                    elif (tktype, tkstr) == (ERRORTOKEN, '$'):
+                        result.extend(
+                                [(NAME, 'superPartial'),
+                                 (OP, '(')])
+                        st_stack.append((st, subject_lp, paren_lp))
+                        subject_lp = 0
+                        paren_lp = 0
+                        st = _PARTIAL_SUBJECT
+                    else:
+                        result.append((tktype, tkstr))
+
+                elif st == _NORMAL:
+                    if (tktype, tkstr) == (ERRORTOKEN, '$'):
+                        result.extend(
+                                [(NAME, 'superPartial'),
+                                 (OP, '(')])
+                        st_stack.append((st, subject_lp, paren_lp))
+                        subject_lp = 0
+                        paren_lp = 0
+                        st = _PARTIAL_SUBJECT
+                    else:
+                        result.append((tktype, tkstr))
+
                 else:
-                    result.append((tktype, tkstr))
+                    raise Exception("Unkown parsing state %d" % st)
             print "from functools import partial as superPartial\n"
             print tokenize.untokenize(result)
             print
